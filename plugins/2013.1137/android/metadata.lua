d4f384ca8da7c0e3ba4526a7d5fca5540f439026
Eric Chen <eric.chen@vungle.com>
2014-05-19 14:34:18 -0700

    updated release with 3.0.4-SNAPSHOT jars

--- plugins/2013.1137/android/metadata.lua
+++ plugins/2013.1137/android/metadata.lua
@@ -10,16 +10,15 @@ local metadata =
 			{
 				"android.permission.INTERNET",
 				"android.permission.WRITE_EXTERNAL_STORAGE",
-				"android.permission.ACCESS_WIFI_STATE",
 				"android.permission.ACCESS_NETWORK_STATE"
 			},
 			usesFeatures = {},
 			applicationChildElements =
 			{
-				[[<activity android:name="com.vungle.sdk.VungleAdvert"
-						    android:configChanges="keyboardHidden|orientation|screenSize"
-						    android:theme="@android:style/Theme.NoTitleBar.Fullscreen"/>]],
-				[[<service android:name="com.vungle.sdk.VungleIntentService"/>]]
+				[[<activity android:name="com.vungle.publisher.FullScreenAdActivity"
+					android:configChanges="keyboardHidden|orientation|screenSize"
+					android:theme="@android:style/Theme.NoTitleBar.Fullscreen"/>]],
+				[[<service android:name="com.vungle.publisher.VungleService" android:exported="false"/>]]
 			},
 		},
 	},
